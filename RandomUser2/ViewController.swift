8221a7b91fe99c83f73b0a5220a30baffcdf88fb
Andrii <k2061190paa@gmail.com>
2020-11-26 12:33:57 +0200

    Initial Commit

---
+++ RandomUser2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUser2
+//
+//  Created by Andrii Pohodin on 26.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
